ed3b454c6d242c36655376ccd98f3eac305fbfd5
phatnguyen1998 <nguyentanphat.12041998@gmail.com>
2024-04-02 22:40:27 +0700

    a

---
+++ docs/assets/status-tap-legacy-CkoqDXFj.js
@@ -0,0 +1,5 @@
+System.register(["./index-legacy-Ba5gTc2l.js"],(function(e,t){"use strict";var n,r,s,o,a;return{setters:[e=>{n=e.aM,r=e.aN,s=e.aO,o=e.aP,a=e.aQ}],execute:function(){
+/*!
+             * (C) Ionic http://ionicframework.com - MIT License
+             */
+e("startStatusTap",(()=>{const e=window;e.addEventListener("statusTap",(()=>{n((()=>{const t=e.innerWidth,n=e.innerHeight,i=document.elementFromPoint(t/2,n/2);if(!i)return;const c=r(i);c&&new Promise((e=>s(c,e))).then((()=>{o((async()=>{c.style.setProperty("--overflow","hidden"),await a(c,300),c.style.removeProperty("--overflow")}))}))}))}))}))}}}));
